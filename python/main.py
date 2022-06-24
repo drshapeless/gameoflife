1354b6337906d6c4ca7db18f85cb8c56fe789827
Jacky Li <drsl@drshapeless.com>
2022-06-25 07:02:34 +0800

    Add python.

---
+++ python/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import tkinter as tk
+root = tk.Tk()
+
+# canvas = tk.Canvas(root, width=550, height=820)
+# canvas.create_rectangle(50, 50, 50, 50, fill="red")
+# canvas.pack(fill=tk.BOTH, expand=1)
+
+root.geometry("500x900")
+root.mainloop()
